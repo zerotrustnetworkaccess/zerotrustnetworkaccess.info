cded892275e98fc3e8219e29afdc9794bb8b5285
Ivan Shvets <shvets.ivan@gmail.com>
2022-02-18 17:34:31 +0300

    feat: timeline func

---
+++ src/assets/js/ztna-timeline.js
@@ -0,0 +1,34 @@
+let timelineItemIndex = 1;
+showTimelineItems(timelineItemIndex);
+
+function plusTimelineItem(n) {
+  showTimelineItems((timelineItemIndex += n));
+}
+
+// Thumbnail image controls
+function currentTimelineItem(n) {
+  showTimelineItems((timelineItemIndex = n));
+}
+
+function showTimelineItems(n) {
+  let i;
+  const timelineItems = document.getElementsByClassName("timeline-item");
+  const timelineYears = document.getElementsByClassName("timelineYear");
+  if (n > timelineItems.length) {
+    timelineItemIndex = 1;
+  }
+  if (n < 1) {
+    timelineItemIndex = timelineItems.length;
+  }
+  for (i = 0; i < timelineItems.length; i++) {
+    timelineItems[i].style.display = "none";
+  }
+  for (i = 0; i < timelineYears.length; i++) {
+    timelineYears[i].className = timelineYears[i].className.replace(
+      " active",
+      ""
+    );
+  }
+  timelineItems[timelineItemIndex - 1].style.display = "block";
+  timelineYears[timelineItemIndex - 1].className += " active";
+}
