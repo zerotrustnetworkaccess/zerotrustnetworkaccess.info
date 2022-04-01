3265d12fc37324640c613763ed016fee9baf0ad5
Ivan Shvets <shvets.ivan@gmail.com>
2022-04-01 16:44:03 +0300

    upd: table separators

--- src/assets/js/subscribe.js
+++ src/assets/js/subscribe.js
@@ -9,22 +9,15 @@ subscribeButton.forEach(
     (el.onclick = async () => {
       const url =
         "https://enclave-networks.activehosted.com/proc.php?u=8&f=8&s=&c=0&m=0&act=sub&v=2&or=99d0a5901dfe9737d4076db8322acd47&email=ivan.s@dashbouquet.com&jsonp=true";
-      // const data = { username: 'example' };
 
       try {
-        const response = await fetch(url);
+        await fetch(url).then((res) => console.log(res));
 
-        if (!response.ok) {
-          throw new Error("Ответ сети был не ok.");
-        }
-
-        const json = await response();
-        console.log("Успех:", JSON.stringify(json));
-        overlay.style.display = "block";
-        popup.style.display = "block";
-        body.style.overflowY = "hidden";
+        // overlay.style.display = "block";
+        // popup.style.display = "block";
+        // body.style.overflowY = "hidden";
       } catch (error) {
-        console.error("Ошибка:", error);
+        console.error(error);
       }
     })
 );
